f343cda1d8a4f4bfe4a0ee91ca465a24223c346a
Brandon LeBlanc <brandon@leblanc.codes>
2017-11-16 14:01:37 -0600

    more script enhancements

--- az-group-deploy.sh
+++ az-group-deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# Automatically populates certain fields in the parameters.
+# _artifactsLocation => The root uri of all the artifacts
+# _artifactsLocationSasToken => The SAS token to append to the URI to authenticate all fetches
+# _artifacts => A list of all the uploaded artifacts
+
 function get_location() {
     azconfig="$HOME/.azure/config"
     if [ -f "$azconfig" ]; then
@@ -38,7 +43,34 @@ function relpath() {
     python -c 'import os.path,sys;sys.stdout.write(os.path.relpath(os.path.abspath(sys.argv[1]), os.path.abspath(sys.argv[2])))' "$1" "${2-${PWD}}"
 }
 
-while getopts "a:l:g:s:f:e:uvd" opt; do
+function md5() {
+    openssl md5 -binary < "$1" | base64 -w0
+}
+
+function append() {
+    json=$1
+    data=$2
+
+    if [[ ${data[0]} == '[' || ${data[0]} == '{' ]]; then
+        jq -c "$data + ." <<< "$json"
+    else
+        jq -c "[$data] + ." <<< "$json"
+    fi
+}
+
+function addKey() {
+    json=$1
+    key=$2
+    data=$3
+
+    if [[ -z "$data" ]]; then
+        data="null"
+    fi
+
+    append "$json" "{ $(escape "$key"): $data }"
+}
+
+while getopts "a:l:g:s:f:e:p:uvd" opt; do
     case $opt in
         a)
             artifactsStagingDirectory=$OPTARG #the folder or sample to deploy
@@ -54,6 +86,9 @@ while getopts "a:l:g:s:f:e:uvd" opt; do
             ;;
         s)
             storageAccountName=$OPTARG #storage account to use for staging, if not supplied one will be created and reused
+            ;;
+        p)
+            persistentStorageGroupName=$OPTARG #resource group for the staging storage account, ARM_Deploy_Staging by default
             ;;
         f)
             templateFile=$OPTARG
@@ -91,22 +126,30 @@ else
     fi
 fi
 
-templateName="$( basename "${templateFile%.*}" )"
-templateDirectory="$(basename "$( dirname "$templateFile")")"
+if [[ -z $persistentStorageGroupName ]]; then
+    persistentStorageGroupName="ARM_Deploy_Staging"
+fi
+
+templateName=$( basename "${templateFile%.*}" )
+templateDirectory=$( basename "$( dirname "$templateFile")" )
 deploymentName="${templateDirectory}-${templateName}-$(strip_uuid)"
 
 if [[ -z $resourceGroupName ]]; then
     resourceGroupName=$(basename "${artifactsStagingDirectory}")
 fi
 
-parameterJson=$( jq '.parameters' "$parametersFile" )
+parameterJson=$( jq -c '.parameters' "$parametersFile" )
+
+if [[ -z $uploadArtifacts ]]; then
+    uploadArtifacts=$(jq 'has("_artifactsLocation") and has("_artifactsLocationSasToken")' <<< "$parameterJson")
+fi
+
 
 if [[ $uploadArtifacts ]]; then
-
     if [[ -z $storageAccountName ]];then
         subscriptionId=$(strip_uuid "$(az account show -o json | jq -r .id)")
         artifactsStorageAccountName="stage$subscriptionId"
-        artifactsResourceGroupName="ARM_Deploy_Staging"
+        artifactsResourceGroupName="$persistentStorageGroupName"
 
         # pass empty resourceGroupName in case user has set a default resource group in settings
         if [[ -z $( az storage account list -g '' -o json | jq -r ".[].name | select(. == $(escape "$artifactsStorageAccountName"))" ) ]]; then
@@ -134,15 +177,28 @@ if [[ $uploadArtifacts ]]; then
 
     blobEndpoint=$( az storage account show -n "$artifactsStorageAccountName" -g "$artifactsResourceGroupName" -o json | jq -r '.primaryEndpoints.blob' )
 
-    parameterJson=$( jq "{_artifactsLocation: {value: $(escape "$blobEndpoint$artifactsStorageContainerName")}, _artifactsLocationSasToken: {value: $(escape "?$sasToken")}} + ." <<< "$parameterJson" )
+    parameterJson=$( addKey "$parameterJson" _artifactsLocation "{value: $(escape "$blobEndpoint$artifactsStorageContainerName")}" )
+    parameterJson=$( addKey "$parameterJson" _artifactsLocationSasToken "{value: $(escape "?$sasToken")}" )
 
     artifactsStagingDirectory=$( sed 's/\/*$//' <<< "$artifactsStagingDirectory" )
 
+    artifacts="[]"
+
     while read -d $'\0' -r filepath; do
         relFilePath=$(relpath "$filepath" "$artifactsStagingDirectory")
-        echo "Uploading file $relFilePath..."
-        az storage blob upload -f "$filepath" --container "$artifactsStorageContainerName" -n "$relFilePath" --account-name "$artifactsStorageAccountName" --account-key "$artifactsStorageAccountKey" --verbose
+        artifacts=$(append "$artifacts" "$(escape "$relFilePath")")
+        md5=$(md5 "$filepath")
+        az_md5=$(az storage blob show --container "$artifactsStorageContainerName" -n "$relFilePath" --account-name "$artifactsStorageAccountName" --account-key "$artifactsStorageAccountKey" -o json | jq -r '.properties.contentSettings.contentMd5')
+        if [[ "$md5" != "$az_md5" ]]; then
+            contentType=$(file "$filepath" -I | cut -d: -f2 | tr -d ' ')
+            echo "Uploading file $relFilePath..."
+            az storage blob upload -f "$filepath" --container "$artifactsStorageContainerName" -n "$relFilePath" --content-md5 "$md5" --content-type "$contentType" --account-name "$artifactsStorageAccountName" --account-key "$artifactsStorageAccountKey" --verbose
+        fi
     done < <(find "$artifactsStagingDirectory" -type f -print0)
+
+    if [[ "$(jq 'has("_artifacts")' <<< "$parameterJson")" ]]; then
+        parameterJson=$(addKey "$parameterJson" _artifacts "{value: $artifacts}")
+    fi
 
     templateRelPath=$(relpath "$templateFile" "$artifactsStagingDirectory")
     templateUri="$blobEndpoint$artifactsStorageContainerName/$templateRelPath?$sasToken"
